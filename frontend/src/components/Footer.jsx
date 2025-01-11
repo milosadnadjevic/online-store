7df1e5ed26c826ec340979fd3c36e6d026e6553b
Milos Adnadjevic <milos@Miloss-Air.lan>
2025-01-10 20:49:14 -0500

    added footer Newsletter OurPolicy

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div>
+        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+            <div>
+                <img src={assets.logo} className='mb-5 w-32' alt="" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
