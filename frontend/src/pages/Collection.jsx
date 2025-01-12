c10eb923839805d73050e009d02cd0aeb0f78865
Milos Adnadjevic <milos@Miloss-Air.lan>
2025-01-11 21:57:22 -0500

    search now working

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -7,7 +7,7 @@ import ProductItem from '../components/ProductItem';
 
 const Collection = () => {
 
-  const { products } = useContext(ShopContext);
+  const { products, search, showSearch } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
   const [filterProducts, setFilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
@@ -38,6 +38,10 @@ const Collection = () => {
 const applyFilter = () => {
 
   let productsCopy = products.slice()
+
+  if (showSearch && search) {
+    productsCopy = productsCopy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()))
+  }
 
   if(category.length > 0) {
     productsCopy = productsCopy.filter(item => category.includes(item.category))
@@ -74,7 +78,7 @@ const sortProduct = (e) => {
 
   useEffect(() => {
     applyFilter()
-  }, [category, subCategory])
+  }, [category, subCategory, search, showSearch])
 
   useEffect(() => {
     sortProduct()
