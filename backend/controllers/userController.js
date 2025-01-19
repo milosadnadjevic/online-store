1caba4a40497e6e336b8046ce265d6504941c48f
Milos Adnadjevic <milos@Miloss-Air.lan>
2025-01-18 21:20:24 -0500

    registerUser function working

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,11 @@
+import validator from 'validator'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import userModel from '../models/userModel.js'
 
+const createToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET)
+}
 
 // Route for user login
 const loginUser = async (req, res) => {
@@ -8,8 +15,43 @@ const loginUser = async (req, res) => {
 
 // Route for user registration
 const registerUser = async (req, res) => {
+    try {
 
-        res.json({msg:"Register API Working"})
+        const {name, email, password} = req.body
+
+        // checking if user already exists or not
+        const exists = await userModel.findOne({email})
+        if (exists) {
+            return res.json({success:false, message: 'User already exists'})
+        }
+
+        // Validating email format and strong password
+        if (!validator.isEmail(email)) {
+            return res.json({success:false, message: 'Please enter a valid email'})
+        }
+        if (password.length < 8) {
+            return res.json({success:false, message: 'Please enter a strong password'})
+        }
+        // hashing user password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const newUser = new userModel({
+            name,
+            email,
+            password:hashedPassword
+        })
+
+        const user = await newUser.save()
+
+        const token = createToken(user._id)
+
+        res.json({success:true, token})
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:error.message})
+    }
 }
 
 //  Route for admin login
