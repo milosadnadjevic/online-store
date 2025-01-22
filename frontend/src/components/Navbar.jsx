e208c8d6828ae8f2ccd329cd72c889589392881b
Milos Adnadjevic <milos@Miloss-Air.lan>
2025-01-21 22:12:22 -0500

    added auth.js route.js cartRoute.js

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -46,14 +46,18 @@ const Navbar = () => {
             <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
 
             <div className='group relative'>
-                <Link to='/login'><img className='w-5 cursor-pointer' src={assets.profile_icon} alt="" /></Link>
+                <img onClick={() => token ? null : navigate('/login')} className='w-5 cursor-pointer' src={assets.profile_icon} alt="" />
+                {/* Dropdown menu */}
+                {token && 
                 <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
-                    <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
-                        <p className='cursor-pointer hover:text-black'>My Profile</p>
-                        <p className='cursor-pointer hover:text-black'>Orders</p>
-                        <p onClick={logout} className='cursor-pointer hover:text-black'>Logout</p>
-                    </div>
+                <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+                    <p className='cursor-pointer hover:text-black'>My Profile</p>
+                    <p onClick={() => navigate('/orders')} className='cursor-pointer hover:text-black'>Orders</p>
+                    <p onClick={logout} className='cursor-pointer hover:text-black'>Logout</p>
                 </div>
+            </div>
+                }
+                
             </div>
             <Link to='/cart' className='relative'>
                     <img src={assets.cart_icon} className='w-5 min-w-5' alt="" />
