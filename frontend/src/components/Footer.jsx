baff69e58391d8f159b6c5fd9d11424588cd0ba4
Milos Adnadjevic <milos@Miloss-Air.lan>
2025-01-25 13:59:29 -0500

    updates/assets

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,7 +7,8 @@ const Footer = () => {
             <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
                 <div>
                     <img src={assets.logo} className='mb-5 w-32' alt="" />
-                    <p className='text-gray-600 w-full md:w-2/3'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, quos.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, quos.
+                    <p className='text-gray-600 w-full md:w-2/3'>Our journey began with a simple idea: to provide a platform where customers
+                     can easily discover, explore, and purchase a wide range of products from the comfort of their homes.
                     </p>
                 </div>
 
